fea8ff2e45aea9e3786cc45c7f56738218da470b
League Student <league@iMac-20.attlocal.net>
2019-12-08 12:58:14 -0800

    Are_You_Happy.java
    Are_You_Happy.java

--- src/_04_are_you_happy/Are_You_Happy.java
+++ src/_04_are_you_happy/Are_You_Happy.java
@@ -10,31 +10,8 @@ public class Are_You_Happy {
 	}
 	if(Happy.equals("no")) {
 		Happy=JOptionPane.showInputDialog("Do you want to be happy?");
-		if("Do you want to be happy?".equals("no")) {
-		JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
-		
-    if(Happy.equals("no")) {
-		if("Do you want to be happy?".equals("yes")) {
-			JOptionPane.showMessageDialog(null, "Change something");
+		if(Happy.equals("no")) {
+		JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");}
+		if(Happy.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "Change something");}
 	} } }
-	 } }
-      }
-
-       
-        . . . . . . . . .
-         . . . . . . . .
-          . . . . . . . 
-           . . . . . . 
-            . . . . .
-             . . . .
-              . . . 
-               . .
-                .
-               . .
-              . . . 
-             . . . .   
-            . . . . .
-           . . . . . .    
-          . . . . . . .      
-         . . . . . . . .       
-        . . . . . . . . .               
